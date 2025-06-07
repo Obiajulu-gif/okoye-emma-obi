fb120741f3251100b8fa1e060a2b66d9e1085cbf
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2025-06-07 17:42:32 +0000

    Add sitemap configuration and robots.txt; include next-sitemap dependency

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// next-sitemap.config.js
+module.exports = {
+	siteUrl: "https://www.emmanuelokoye.xyz", // <-- your live domain
+	generateRobotsTxt: true, // generates robots.txt as well
+	sitemapSize: 7000, // optional
+};
